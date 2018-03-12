eec0599c0b5787853ea1f141cbfdd035a4aa4edf
Hugo <hugo.paigneau@etu.utc.fr>
2018-03-12 22:56:18 +0100

    Create ajouter_gare.php

---
+++ NF17-SCF/admin/ajouter_gare.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta http-equiv="Content-Type" content="text/html
+    ; charset=UTF-8" />
+    <link href='http://fonts.googleapis.com/css?family=Roboto:400,100,300,500,700,900' rel='stylesheet' type='text/css'>
+    <title>Société De Chemins de Fer</title>
+    <link rel="stylesheet" href="main.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+  </head>
+  <body>
+    <div class="container text-center">
+      <h1>Vous avez ajouté une gare !</h1>
+    </div>
+    <?php
+      $user = 'nf17p050';
+      $password = 'klfRl2NH';
+      $connexion = new PDO('pgsql:host=tuxa.sme.utc ; dbname=dbnf17p050; port=5432',$user,$password, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+
+
+
+      $connexion=null;
+     ?>
+    <div>
+    </div>
+  </body>
+</html>
